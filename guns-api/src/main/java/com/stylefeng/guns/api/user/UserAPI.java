94106bfc12ca538ab482a2ce560519a238e3fc9e
zhuangsen <zhuangsen@live.com>
2018-11-08 17:49:06 +0800

    update

--- guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -2,4 +2,8 @@ package com.stylefeng.guns.api.user;
 
 public interface UserAPI {
     boolean login(String username, String password);
+
+    boolean register(UserModel userModel);
+
+    boolean checkUserName(String userName);
 }
