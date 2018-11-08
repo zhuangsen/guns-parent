e7f4bff553f7c29411ce4936386d9664c1b8f075
zhuangsen <zhuangsen@live.com>
2018-11-08 17:02:17 +0800

    update

---
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/dao/TbHouseMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.system.dao;
+
+import com.stylefeng.guns.modular.system.model.TbHouse;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 房屋管理表 Mapper 接口
+ * </p>
+ *
+ * @author madison
+ * @since 2018-11-08
+ */
+public interface TbHouseMapper extends BaseMapper<TbHouse> {
+
+}
